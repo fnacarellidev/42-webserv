4b460890c4ee97e0643ac11be9741f3adba1e5d6
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-04-03 16:26:14 -0300

    feat: test files and reworked in main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,12 @@
 #include "../includes/Response.hpp"
 
-int main() {
-	Response	r(200, "./index.html");
+int main(int argc, char **argv) {
+	if (argc != 2)
+		return (1);
+
+	Response	r(200, argv[1]);
 
 	std::cout << r.getFullReponse();
 	
-	return(0);
+	return (0);
 }
