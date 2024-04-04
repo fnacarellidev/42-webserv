91ef87dad1a584bd4911977900a0ad1dd1dfdb29
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-04-04 19:30:59 -0300

    refactor: don't need those includes anymore

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -1,12 +1,7 @@
 #pragma once
 #include <iostream>
 #include <list>
-#include "../includes/HttpStatus.hpp"
 #include "../includes/ServerConfig.hpp"
-
-#define GET_OK 0b0001
-#define POST_OK 0b0010
-#define DELETE_OK 0b0100
 
 enum Methods {
 	GET,
--- includes/RouteConfig.hpp
+++ includes/RouteConfig.hpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <utility>
 
+#define GET_OK 0b0001
+#define POST_OK 0b0010
+#define DELETE_OK 0b0100
+
 class RouteConfig
 {
 	private:
