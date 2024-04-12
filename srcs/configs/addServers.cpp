83c07596c59d36ddd1d2466fae4333b3971c9ae9
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-04-12 09:21:58 -0300

    fix: infinite loop in addRoutes

--- srcs/configs/addServers.cpp
+++ srcs/configs/addServers.cpp
@@ -93,5 +93,7 @@ void    addRoutes(std::ifstream& file, std::string& line, ServerConfig& server) 
 			server._routes.back()->_dirList = (splited[1] == "on");
 		else if (routeMap[splited[0]] == Route::PATH)
 			server._routes.back()->setPath(removeExtraSlashes(splited[1]));
+		std::getline(file, line);
+		trim(line, "\t \n");
 	}
 }
