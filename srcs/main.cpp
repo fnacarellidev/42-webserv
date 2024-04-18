af316a9819f723d0ea64f580652be9d43c7be333
adantas- <adantas-@student.42sp.org.br>
2024-04-18 15:40:03 -0300

    refac: refactor done, but still have that while(true)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,7 +2,6 @@
 #include "../includes/Request.hpp"
 #include "../includes/WebServer.hpp"
 
-#define OPT 1
 #ifndef CONNECTIONS // maximum number of newSockets in the poll
 #	define CONNECTIONS 1000
 #endif
@@ -13,7 +12,7 @@
 #	define POLL_TIMEOUT 10 * 1000
 #endif
 #ifndef BUFFER_SIZE // maximum read for a request
-#	define BUFFER_SIZE 4096
+#	define BUFFER_SIZE 1048576
 #endif
 
 static void	closeAll(std::vector<struct pollfd>& pollFds) {
@@ -32,15 +31,8 @@ static bool	invalidInputs(int argc, char **argv) {
 	return false;
 }
 
-// static void	waitPoll(std::vector<struct pollfd>& pollFds) {
-// 	if (poll(&pollFds[0], pollFds.size(), POLL_TIMEOUT) < 0) {
-// 		perror("poll");
-// 		throw std::runtime_error("");
-// 	}
-// }
-
 static void	setupSockets(WebServer& config, std::vector<int>& serverFds) throw(std::runtime_error) {
-	int opt = 1, serverFd;
+	int	opt = 1, serverFd;
 
 	for (size_t i = 0; i < config.servers.size(); i++) {
 		serverFd = socket(AF_INET, SOCK_STREAM, 0);
@@ -48,7 +40,6 @@ static void	setupSockets(WebServer& config, std::vector<int>& serverFds) throw(s
 			perror("socket");
 			goto error_happen;
 		}
-		// if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, (int*)OPT, 4)) {
 		if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, 4)) {
 			perror("setsockopt");
 			goto error_happen;
@@ -87,7 +78,6 @@ int main(int argc, char **argv) {
 		config.servers.push_back(ServerConfig());
 		config.servers.back().routes.push_back(new RouteConfig());
 	}
-
 	try {
 		setupSockets(config, serverFds);
 	} catch (std::runtime_error& e) {
@@ -115,21 +105,26 @@ int main(int argc, char **argv) {
 					if (pollFds.size() < CONNECTIONS)
 						pollFds.push_back((struct pollfd){.fd = newSocket, .events = POLLIN | POLLOUT});
 					else {
+						Response resFull(HttpStatus::SERVICE_UNAVAILABLE);
+
 						std::cerr << "We are full" << std::endl;
+						send(newSocket, resFull.response(), resFull.size(), MSG_CONFIRM);
 						close(newSocket);
 					}
-				} else {
+				}
+				else {
 					char	*buffer = (char*)std::calloc(BUFFER_SIZE, sizeof(char));
 					int	ret = recv(pollFds[i].fd, buffer, BUFFER_SIZE, 0);
 
 					if (ret < 0) {
-						Response res(500);
+						Response resErr(HttpStatus::SERVER_ERR);
 
 						std::cerr << "recv error" << std::endl;
-						send(pollFds[i].fd, res.response(), res.size(), MSG_CONFIRM);
+						send(pollFds[i].fd, resErr.response(), resErr.size(), MSG_CONFIRM);
 						close(pollFds[i].fd);
 						pollFds.erase(pollFds.begin() + i);
-					} else {
+					}
+					else {
 						buffer[ret] = 0;
 
 						Request req(buffer, config.servers);
@@ -147,56 +142,3 @@ int main(int argc, char **argv) {
 	closeAll(pollFds);
 	return 0;
 }
-
-// static void	addResponseSocket(std::vector<struct pollfd>& pollFds, int serverFd) {
-// 	int newSocket = accept(serverFd, NULL, NULL);
-
-// 	if (newSocket < 0) {
-// 		perror("accept");
-// 		return ;
-// 	}
-// 	if (pollFds.size() < CONNECTIONS) {
-// 		pollFds.push_back((struct pollfd){.fd = newSocket, .events = POLLIN | POLLOUT});
-// 		return ;
-// 	}
-
-// 	Response resFull(HttpStatus::SERVICE_UNAVAILABLE);
-
-// 	std::cerr << "Queue size: " << pollFds.size() << std::endl;
-// 	send(newSocket, resFull.response(), resFull.size(), MSG_CONFIRM);
-// 	close(newSocket);
-// }
-
-// static void	processPoll(std::vector<struct pollfd>& pollFds, std::vector<int>& serverFds, WebServer& config) {
-// 	std::string buffer;
-// 	for (size_t i = 0; i < pollFds.size(); i++) {
-// 		if (pollFds[i].revents & POLLIN) {
-// 			if (pollFds[i].fd == serverFds[i])
-// 				addResponseSocket(pollFds, serverFds[i]);
-// 			else {
-// 				char temp[BUFFER_SIZE] = {0};
-// 				int ret = recv(pollFds[i].fd, temp, BUFFER_SIZE, 0);
-
-// 				if (ret < 0) {
-// 					Response res(HttpStatus::SERVER_ERR);
-
-// 					std::cerr << "recv error" << std::endl;
-// 					send(pollFds[i].fd, res.response(), res.size(), MSG_CONFIRM);
-// 					close(pollFds[i].fd);
-// 					pollFds.erase(pollFds.begin() + i);
-// 				} else {
-// 					buffer[ret] = 0;
-
-// 					Request req(buffer, config.servers);
-// 					Response res = req.runRequest();
-
-// 					send(pollFds[i].fd, res.response(), res.size(), MSG_CONFIRM);
-// 					close(pollFds[i].fd);
-// 					pollFds.erase(pollFds.begin() + i);
-// 				}
-// 			}
-// 		} else if (pollFds[i].revents & POLLOUT) {
-			
-// 		}
-// 	}
-// }
