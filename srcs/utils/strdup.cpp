b5341ece6ffa94d1402bddadae9b900296a8039b
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-04-25 15:46:22 -0300

    fix: systemcall execve when doing GET method in a cgi

--- srcs/utils/strdup.cpp
+++ srcs/utils/strdup.cpp
@@ -1,7 +1,7 @@
 #include "../../includes/utils.hpp"
 
 char*	utils::strdup(std::string str) {
-	char* dup = (char *) std::calloc(str.size(), 1);
+	char* dup = (char *) std::calloc(str.size() + 1, 1);
 
 	for (size_t i = 0; i < str.size(); ++i)
 		dup[i] = str[i];
